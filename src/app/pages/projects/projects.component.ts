a2aa3b779c3cba39eca4035767806adc4219b912
Abhiranjan Mukherjee <abhiranjan.mukherjee@in.bosch.com>
2024-10-05 02:11:53 +0530

    Design Project Page

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
-
+  templates = [
+    { name: 'Template 1', description: 'Template 1 description' },
+    { name: 'Template 2', description: 'Template 2 description' },
+    { name: 'Template 3', description: 'Template 3 description' },
+    { name: 'Template 4', description: 'Template 4 description' }
+  ];
 }
