ead8b3c927f926258fa9584d1f9a53d21a8a58f2
Abhiranjan Mukherjee <abhiranjan.mukherjee@in.bosch.com>
2024-10-09 00:14:35 +0530

    Fixing Project Page Design

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -10,6 +10,10 @@ export class ProjectsComponent {
     { name: 'Template 1', description: 'Template 1 description' },
     { name: 'Template 2', description: 'Template 2 description' },
     { name: 'Template 3', description: 'Template 3 description' },
-    { name: 'Template 4', description: 'Template 4 description' }
+    { name: 'Template 4', description: 'Template 4 description' },
+    { name: 'Template 5', description: 'Template 5 description' },
+    { name: 'Template 6', description: 'Template 6 description' },
+    { name: 'Template 7', description: 'Template 7 description' },
+    { name: 'Template 8', description: 'Template 8 description' }
   ];
 }
