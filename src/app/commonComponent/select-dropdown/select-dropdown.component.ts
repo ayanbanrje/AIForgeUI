83e9ae9a7b537d6eb3309adaada3c22cfd5db6e1
Ayan Bandyopadhyay <ayan.bandyopadhyay@in.bosch.com>
2024-10-12 09:07:14 +0530

    working

---
+++ src/app/commonComponent/select-dropdown/select-dropdown.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { IDropdownSettings } from 'ng-multiselect-dropdown';
+
+@Component({
+  selector: 'app-select-dropdown',
+  templateUrl: './select-dropdown.component.html',
+  styleUrl: './select-dropdown.component.scss'
+})
+export class SelectDropdownComponent {
+  dropdownList: { item_id: number; item_text: string }[] = [];
+  selectedItems : { item_id: number; item_text: string }[] = [];
+  
+  dropdownSettings : IDropdownSettings = {
+    singleSelection: false,
+    idField: 'item_id',
+    textField: 'item_text',
+    selectAllText: 'Select All',
+    unSelectAllText: 'UnSelect All',
+    itemsShowLimit: 3,
+    allowSearchFilter: true
+  };
+
+  ngOnInit() {
+    this.dropdownList = [
+      { item_id: 1, item_text: 'Mumbai' },
+      //{ item_id: 2, item_text: 'Bangaluru' },
+      // { item_id: 3, item_text: 'Pune' },
+      // { item_id: 4, item_text: 'Navsari' },
+      // { item_id: 5, item_text: 'New Delhi' }
+    ];
+    this.selectedItems = [
+      // { item_id: 3, item_text: 'Pune' },
+      // { item_id: 4, item_text: 'Navsari' }
+    ];
+    
+  }
+  onItemSelect(item: any) {
+    console.log(item);
+  }
+  onSelectAll(items: any) {
+    console.log(items);
+  }
+}
