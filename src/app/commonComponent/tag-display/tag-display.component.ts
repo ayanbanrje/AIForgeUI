3a3f0358d2c2be2a17372f1115f9bef5e5b949d3
Rauniyar Badri Prasad (SDS/EDS) <bqu1kor@bosch.com>
2024-10-16 11:29:49 +0530

    tags

--- src/app/commonComponent/tag-display/tag-display.component.ts
+++ src/app/commonComponent/tag-display/tag-display.component.ts
@@ -8,7 +8,7 @@ import { Component, Input } from '@angular/core';
 export class TagDisplayComponent {
   @Input() Tags=[]
   openAllTagsModal:boolean=false
-
+  @Input() modalHeader=''
   openModal(){
     this.openAllTagsModal=true
   }
